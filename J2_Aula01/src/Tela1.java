56f0fd0c95a3475e9d188cae076839f127b0ed51
Tiago Seibel <tiagoseibel@gmail.com>
2019-08-29 21:49:08 -0300

    projeto 1 - eventos de mouse

--- J2_Aula01/src/Tela1.java
+++ J2_Aula01/src/Tela1.java
@@ -1,7 +1,10 @@
 
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
 public class Tela1 extends JFrame {
@@ -22,6 +25,13 @@ public class Tela1 extends JFrame {
         this.botao = new JButton("OK");
         this.botao.setBounds(0, 0, 100, 50);
         add(botao);
+
+        botao.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null, txt1.getText());
+            }
+        });
 
         setVisible(true);
     }
