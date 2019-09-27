6b967c11d480ae2c5781dbbd1862a5f7a2298526
tiagoseibel <tiagoseibel@gmail.com>
2019-09-26 21:07:01 -0300

    Aula 05 - Trabalhando com bibliotecas de terceiros (Commons CSV) - Parte II

--- J2_Aula05/src/main/LerCSV.java
+++ J2_Aula05/src/main/LerCSV.java
@@ -2,9 +2,12 @@ package main;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
+import java.io.Writer;
 import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 
 public class LerCSV {
@@ -27,5 +30,21 @@ public class LerCSV {
             String titulo = i.get("Titulo");
             System.out.println(autor + " - " + titulo);
         }
+        
+        // Registrar dados
+        Writer w = new FileWriter("novos-livros.csv");
+        
+        CSVPrinter print 
+            = new CSVPrinter(w, 
+                    CSVFormat.DEFAULT.withHeader("Autor", "Titulo")
+                    .withDelimiter(';')
+            );
+        // Linha 1
+        print.printRecord("CJ Date", "Introdução a Sistemas de Bancos de Dados");
+        // Linha 2
+        print.printRecord("Laudon e Laudon", "Sistemas de Informações Gerenciais");
+        // Linha 3
+        print.printRecord("Turban", "Tecnologia da Informação para Gestão");
+        w.close();
     }
 }
